c194e29494a21d95e55f6412cbfa7a8fac6ae892
alexander-io <abharris@pugetsound.edu>
2017-08-11 14:53:04 -0700

    ++move request handler for index.html from server.js to scripts/dependencies.js

--- server.js
+++ server.js
@@ -7,14 +7,9 @@ server.listen(8080, function(){
   console.log('listening on port 8080')
 })
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/' + 'index.html')
-})
-
 dependencies.requests(app, path)
 
 io.on('connection', function(socket) {
-
   socket.emit('news', {hello : 'world'})
   socket.on('my other event', function(data) {
     console.log(data);
