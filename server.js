440d8a8b183b3610b10268fb9cda1ed058f91b6b
alexander-io <abharris@pugetsound.edu>
2017-08-11 14:51:23 -0700

    ++server.js, prepend 'ss = require('socket.io-stream')'

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-let express = require('express'), path = require('path'), dependencies = require(__dirname + '/scripts/dependencies.js')
+let express = require('express'), path = require('path'), dependencies = require(__dirname + '/scripts/dependencies.js'), ss = require('socket.io-stream')
 let app = express()
 let server = require('http').Server(app)
 let io = require('socket.io')(server)
@@ -18,5 +18,5 @@ io.on('connection', function(socket) {
   socket.emit('news', {hello : 'world'})
   socket.on('my other event', function(data) {
     console.log(data);
-  })  
+  })
 })
