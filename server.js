65e1565e73e2d8417d72fa5c2f807719a2f80e45
alexander-io <abharris@pugetsound.edu>
2017-08-11 14:49:06 -0700

    ++ install socket.io-stream

--- server.js
+++ server.js
@@ -13,33 +13,10 @@ app.get('/', function(req, res) {
 
 dependencies.requests(app, path)
 
-// app.get('/style.css', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/public/css/style.css'))
-// })
-//
-// app.get('/materialize.min.css', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/css/materialize.min.css'))
-// })
-//
-// app.get('/materialize.min.js', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/js/materialize.min.js'))
-// })
-//
-// app.get('/jquery.min.js', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/node_modules/jquery/dist/jquery.min.js'))
-// })
-//
-// app.get('/fonts/roboto/Roboto-Regular.woff2', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff2'))
-// })
-//
-// app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff'))
-// })
-
 io.on('connection', function(socket) {
+
   socket.emit('news', {hello : 'world'})
   socket.on('my other event', function(data) {
     console.log(data);
-  })
+  })  
 })
