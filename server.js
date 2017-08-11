68fbfd432353981db1cad6dc1d6f0edbd4ab6932
alexander-io <abharris@pugetsound.edu>
2017-08-11 14:43:03 -0700

    ++export module for providing requested dependencies

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-let express = require('express'), path = require('path')
+let express = require('express'), path = require('path'), dependencies = require(__dirname + '/scripts/dependencies.js')
 let app = express()
 let server = require('http').Server(app)
 let io = require('socket.io')(server)
-
-
 
 server.listen(8080, function(){
   console.log('listening on port 8080')
@@ -13,29 +11,31 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/' + 'index.html')
 })
 
-app.get('/style.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/style.css'))
-})
+dependencies.requests(app, path)
 
-app.get('/materialize.min.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/css/materialize.min.css'))
-})
-
-app.get('/materialize.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/js/materialize.min.js'))
-})
-
-app.get('/jquery.min.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/node_modules/jquery/dist/jquery.min.js'))
-})
-
-app.get('/fonts/roboto/Roboto-Regular.woff2', function(req, res) {
-  res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff2'))
-})
-
-app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
-  res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff'))
-})
+// app.get('/style.css', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/public/css/style.css'))
+// })
+//
+// app.get('/materialize.min.css', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/css/materialize.min.css'))
+// })
+//
+// app.get('/materialize.min.js', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/js/materialize.min.js'))
+// })
+//
+// app.get('/jquery.min.js', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/node_modules/jquery/dist/jquery.min.js'))
+// })
+//
+// app.get('/fonts/roboto/Roboto-Regular.woff2', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff2'))
+// })
+//
+// app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff'))
+// })
 
 io.on('connection', function(socket) {
   socket.emit('news', {hello : 'world'})
